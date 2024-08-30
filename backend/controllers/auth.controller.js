99204c125ebb84f9dee274f8250e80fbd3439e07
visvius <vishal.singhofficial1@gmail.com>
2024-08-30 23:47:05 +0530

    Project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: "Signup"
+    })
+};
+export const login = async (req, res) => {
+    res.json({
+        data: "Login"
+    })
+};
+export const logout = async (req, res) => {
+    res.json({
+        data: "logout"
+    })
+};
---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected !`)
+
+    } catch(error) {
+        console.log(`Error connecting to mongoDB: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectMongoDB;
