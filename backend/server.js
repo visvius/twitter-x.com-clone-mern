a413230f66f29ca6a63996f868c2e3fb63bb5e96
visvius <vishal.singhofficial1@gmail.com>
2024-08-31 13:33:47 +0530

    Controllers added

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
