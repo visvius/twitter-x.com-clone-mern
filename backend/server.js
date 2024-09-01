77388878d2a9f7a7f6d24dfd88067e4509e030e1
visvius <vishal.singhofficial1@gmail.com>
2024-09-02 00:26:14 +0530

    Notification routes and controllers added.

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+
 import connectMongoDB from "./db/connectMongoDB.js";
+
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 dotenv.config();
 
@@ -25,6 +28,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(5000, () => {
     console.log(`Server is running on port ${PORT}`);
